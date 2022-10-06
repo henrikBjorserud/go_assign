4d48677cd282425a755e33506002b11bcf813e3c
henrikBjorserud <henrikbjorserud@gmail.com>
2022-10-06 11:06:36 +0200

    Added rotation and scheduling for second part of day

--- verkstan.go
+++ verkstan.go
@@ -6,30 +6,38 @@ import (
     "math/rand"
     "time"
 )
-
+// The struct that is used to create a schedule for three worrkers
 type three struct {
     Jk string
     MbAbAg string
     KuGlFb string
 }
-
+// The struct that is used to create a schedule for four workers
 type four struct {
     Jk string
     MbAbAg string
     Ku string
     GlFb string
 }
+// Rotate names
+func rotate_names(names []string) []string {
+    var newdata []string
+    first := names[:1]
+    newdata = append(newdata,names[1:]...)
+    newdata = append(newdata,first...)
+    return newdata
+}
 
+// Assign names to three empty slots
 func newThree(names[]string) *three {
     shift := three{}
     shift.Jk = names[0]
     shift.MbAbAg = names[1]
     shift.KuGlFb = names[2]
 
-    fmt.Println(shift)
     return &shift
 }
-
+// Assign names to four empty slots
 func newFour(names[]string) *four {
     shift := four{}
     shift.Jk = names[0]
@@ -37,16 +45,15 @@ func newFour(names[]string) *four {
     shift.Ku = names[2]
     shift.GlFb = names[3]
 
-    fmt.Println(shift)
     return &shift
 }
+
 // The main function for making a schedule for verkstan
 func name_scan(numPers int) []string {
 
      names := make([]string, numPers)
 
     for i := 0; i < numPers;{
-        fmt.Println(i)
         fmt.Println("Skriv ett namn och tryck enter: ")
         var name string
         fmt.Scanln(&name)
@@ -55,7 +62,7 @@ func name_scan(numPers int) []string {
     }
         return names
 }
-
+// Put names in random order
 func shuffle_names(names []string) []string {
     rand.Seed(time.Now().UnixNano())
     rand.Shuffle(len(names), func(i, j int) {names[i], names[j] = names[j], names[i]})
@@ -75,13 +82,23 @@ func main() {
 
     if len(names) == 3 {
         FM := newThree(names)
+        fmt.Println("Before Lunch:")
         fmt.Printf("%+v", FM)
-
+        rotated_names := rotate_names(names)
+        fmt.Println("\nAfter Lunch")
+        EM := newThree(rotated_names)
+        fmt.Printf("%+v", EM)
+        fmt.Println("\n")
+    }
 
     if len(names) == 4 {
         FM := newFour(names)
+        fmt.Println("Before Lunch:")
         fmt.Printf("%+v", FM)
-
-
+        rotated_names := rotate_names(names)
+        EM := newFour(rotated_names)
+        fmt.Println("\nAfter Lunch:")
+        fmt.Printf("%+v", EM)
+        fmt.Println("\n")
     }
 }
